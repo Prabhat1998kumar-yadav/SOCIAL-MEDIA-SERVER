c8d488053fe4200fa0d80dabe398afca0135f890
prabhat kumar yadav <prabhat1298@gmail.com>
2025-03-13 15:22:43 +0530

    "server"

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ const app=express();
 app.use(express.json({limit:"10mb"}))
 
 app.use(cookieParser())
-app.use(cors());
+app.use(cors({
+    secure:true,
+    origin:"https://social-media-client-a3xa.onrender.com"
+}));
 
 app.use("/auth",authRouter);
 app.use("/posts",postsRouter);
