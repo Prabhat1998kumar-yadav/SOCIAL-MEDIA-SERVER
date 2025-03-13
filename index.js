0545f19dead3b560f02f45dcd30809c0a69838ad
prabhat kumar yadav <prabhat1298@gmail.com>
2025-03-13 19:12:22 +0530

    previous

--- index.js
+++ index.js
@@ -25,12 +25,13 @@ const app=express();
 app.use(express.json({limit:"10mb"}))
 
 app.use(cookieParser())
-let origin="http://localhost:5173"
-if(process.env.NODE_ENV === "production"){
-    origin="https://social-media-client-production.up.railway.app"
-}
+// let origin="http://localhost:5173"
+// if(process.env.NODE_ENV === "production"){
+//     origin="https://social-media-client-production.up.railway.app"
+// }
+
 app.use(cors({
-    origin,    
+    origin:"https://social-media-client-production.up.railway.app",    
     credentials: true, 
 }));
 
